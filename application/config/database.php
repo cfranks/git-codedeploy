a03afedc84049cd61516e84e51cf07bffb92a2eb
cfranks <casey.franks@informaticsinc.com>
2020-06-09 20:52:21 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ return [
     'connections' => [
         'concrete' => [
             'driver' => 'c5_pdo_mysql',
-            'server' => 'tdhy2n0x4yw8nd.cszobye4yuyb.us-east-1.rds.amazonaws.com',
+            'server' => 'ad1iug0r28c9f3n.cuharxtmosio.us-east-1.rds.amazonaws.com',
             'database' => 'aadprtweb2_db',
             'username' => 'aadprtweb_usr',
             'password' => 'FvZN59QHnKVu',
